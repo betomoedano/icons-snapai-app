a4b85a500102d48489be38ae757499d5cbdbe9be
betomoedano <betomoedano@outlook.com>
2025-07-27 18:28:28 -0500

    add icon

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
 
 export default function App() {
+  const theme = useColorScheme();
+  const backgroundColor = theme === "dark" ? "#000" : "#fff";
+  const color = theme === "dark" ? "#fff" : "#000";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={{ color }}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
